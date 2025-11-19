5481b611ca19c841dc5c41ea14d10a10c728524c
olaleketimothy08 <olaleketimothy08@gmail.com>
2025-11-19 22:26:24 +0000

    changed

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navItems = document.querySelector('.nav-items');
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode")
+}
 
-menuToggle.addEventListener('click', function(){
-navItems.classList.toggle('active'); 
-menuToggle.classList.toggle('open');
-});
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
